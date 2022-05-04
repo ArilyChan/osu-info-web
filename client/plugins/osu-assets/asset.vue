ff23b4d161a431fb6b39b6dd3b28ab8a065df6b0
arily <arily@arily.moe>
2022-05-05 07:33:29 +0900

    remove unused

--- client/plugins/osu-assets/asset.vue
+++ client/plugins/osu-assets/asset.vue
@@ -45,32 +45,5 @@ export default {
       }
     }
   }
-  // data () {
-  //   // const skin = '~/assets/skins/- Nekoha Shizuku -'
-  //   const score = Object.entries({ 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, comma: ',', dot: '.', percent: '%', x: 'x' })
-  //     .map(([k, v]) => [v, require(`~/assets/skins/- Nekoha Shizuku -/score-${k}.png`)])
-  //     .reduce((acc, [k, v]) => { acc[k] = v; return acc }, {})
-  //   return {
-  //     assets: {
-  //       score,
-  //       ranking: {
-  //       }
-  //     }
-  //   }
-  // },
-  // beforeMount () {
-  //   const path = `./assets/skins/${this.skin}`
-  //   this.assets.ranking = {
-  //     hit300: require(`${path}/hit300.png`),
-  //     hit100: require(`${path}/hit100.png`),
-  //     hit50: require(`${path}/hit50.png`),
-  //     hit0: require(`${path}/hit0.png`),
-  //     hit300g: require(`${path}/hit300g.png`),
-  //     hit300k: require(`${path}/hit300k.png`),
-  //     maxCombo: require(`${path}/ranking-maxcombo.png`),
-  //     accuracy: require(`${path}/ranking-accuracy.png`)
-  //   }
-  //   console.log(this.assets)
-  // }
 }
 </script>
